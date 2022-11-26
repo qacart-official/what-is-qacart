0f87d18629dfecc92e32d4b9ce1a9f4db7f6df9d
qacart-official <hatem.hatamleh@qacart.com>
2022-11-27 00:44:26 +0100

    📦 NEW: Adding color to the terminal

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
 #!/usr/bin/env node
 const packageJson = require("./package.json");
-const welcome = require("cli-welcome");
+const chalk = require("chalk");
+const log = console.log;
 
-// console.log(`
-// NAME: ${packageJson.name}
-// VERSION: ${packageJson.version}
-// DESCRIPTION: ${packageJson.description}
-// `);
+log(`
+${chalk.bold.bgYellow(`   QAcart   `)} ${packageJson.version}
 
-welcome({
-   title: `QAcart`,
-   description: packageJson.description,
-   version: packageJson.version,
-   bgColor: `#FADC00`,
-   color: `#000000`,
-   bold: true,
-   clear: false,
-});
+${chalk.italic(
+   `Software testing is easier in Arabic ... Software testing is easier with QAcart`,
+)}
+ 
+${chalk.bgBlue(" Top Projects ")}
 
-console.log(`
+- Selenium: ${chalk.dim(
+   `https://qacart.com/course/complete-selenium-framework-for-todo-application`,
+)}
+- Cypress: ${chalk.dim(`https://qacart.com/course/cypress-todo-project`)}
+- RestAssured: ${chalk.dim(
+   `https://qacart.com/course/rest-assured-todo-project`,
+)}
 
-Top Projects:
-- Selenium: https://qacart.com/course/complete-selenium-framework-for-todo-application
-- Cypress: https://qacart.com/course/cypress-todo-project
-- RestAssured: https://qacart.com/course/rest-assured-todo-project
+${chalk.bgGreen(" Free courses ")}
 
-Free Courses:
-- Manual testing: https://qacart.com/course/manual-testing-bootcamp-2022
-- ISTQB foundation level: https://qacart.com/course/istqb-foundation-level
-- Postman: https://qacart.com/course/postman
-- SQL: https://qacart.com/course/complete-sql-course
-- Programming languages: https://qacart.com/course/programming-languages-for-testers
-- Testing tools: https://qacart.com/course/testing-tools
-- Selenium: https://qacart.com/course/selenium-webdriver
-- Cypress: https://qacart.com/course/cypressio
-- Playwright: https://qacart.com/course/playwright-javascript
-- RestAssured: https://qacart.com/course/rest-assured-101
+- Manual testing: ${chalk.dim(
+   `https://qacart.com/course/manual-testing-bootcamp-2022`,
+)}
+- ISTQB foundation level: ${chalk.dim(
+   `https://qacart.com/course/istqb-foundation-level`,
+)}
+- Postman: ${chalk.dim(`https://qacart.com/course/postman`)}
+- SQL: ${chalk.dim(`https://qacart.com/course/complete-sql-course`)}
+- Programming languages: ${chalk.dim(
+   `https://qacart.com/course/programming-languages-for-testers`,
+)}
+- Testing tools: ${chalk.dim(`https://qacart.com/course/testing-tools`)}
+- Selenium: ${chalk.dim(`https://qacart.com/course/selenium-webdriver`)}
+- Cypress: ${chalk.dim(`https://qacart.com/course/cypressio`)}
+- Playwright: ${chalk.dim(`https://qacart.com/course/playwright-javascript`)}
+- RestAssured: ${chalk.dim(`https://qacart.com/course/rest-assured-101`)}
 
-Follow us:
-- Facebook: https://www.facebook.com/QAcartAcademy
-- LinkedIn: https://www.linkedin.com/company/qacart-official/
-- Youtube: https://www.youtube.com/channel/UCaGo8x7uqxMK5A08AI8s6IQ
+${chalk.bgCyan(" Follow us ")}
 
-Software testing is easier in Arabic ... Software testing is easier with QAcart
-
+- Facebook: ${chalk.dim(`https://www.facebook.com/QAcartAcademy`)}
+- LinkedIn: ${chalk.dim(`https://www.linkedin.com/company/qacart-official/`)}
+- Youtube: ${chalk.dim(
+   `https://www.youtube.com/channel/UCaGo8x7uqxMK5A08AI8s6IQ`,
+)}
 `);
