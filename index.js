c463f1958f58e465b1ad0dfbc6a28b7acf6786f3
qacart-official <hatem.hatamleh@qacart.com>
2022-11-26 16:40:15 +0100

    first version

---
+++ index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+console.log(`
+QAcart
+Innovative Software Test Trainings
+Founder - Hatem Hatamleh
+
+Top Projects:
+- Selenium: https://qacart.com/course/complete-selenium-framework-for-todo-application
+- Cypress: https://qacart.com/course/cypress-todo-project
+- RestAssured: https://qacart.com/course/rest-assured-todo-project
+
+Free Courses:
+- Manual testing: https://qacart.com/course/manual-testing-bootcamp-2022
+- ISTQB foundation level: https://qacart.com/course/istqb-foundation-level
+- Postman: https://qacart.com/course/postman
+- SQL: https://qacart.com/course/complete-sql-course
+- Programming languages: https://qacart.com/course/programming-languages-for-testers
+- Testing tools: https://qacart.com/course/testing-tools
+- Selenium: https://qacart.com/course/selenium-webdriver
+- Cypress: https://qacart.com/course/cypressio
+- Playwright: https://qacart.com/course/playwright-javascript
+- RestAssured: https://qacart.com/course/rest-assured-101
+
+Follow us:
+- Facebook: https://www.facebook.com/QAcartAcademy
+- LinkedIn: https://www.linkedin.com/company/qacart-official/
+- Youtube: https://www.youtube.com/channel/UCaGo8x7uqxMK5A08AI8s6IQ
+
+Software testing is easier in Arabic ... Software testing is easier with QAcart
+
+`);
